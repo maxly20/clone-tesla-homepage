03c66dda21213b5130f3fe95fb5e3f4ab84f2075
maxly20 <maxly2011@gmail.com>
2021-01-24 12:59:02 +1100

    fix readme file

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A small project to clone the Tesla Australia Homepage, using HTML and CSS and some JavaScript.
 
+This project is not mobile responsive yet. This feature will be added later.
+
 Deployed live at https://clone-tesla-homepage.netlify.app
 
 ![](SitePreview.jpg)
--- app.js
+++ app.js
@@ -10,3 +10,24 @@ const navSlide = () => {
 
 navSlide();
 
+// ADDING GSAP
+
+gsap.from('#section_0 .content h1', {
+  opacity: 0,
+  duration: 1,
+  delay: 0.5,
+  y: 20,
+});
+gsap.from('#section_0 .content p', {
+  opacity: 0,
+  duration: 1,
+  delay: 1.2,
+  y: 10,
+});
+gsap.from('#section_0 .buttons .options', {
+  opacity: 0,
+  duration: 1,
+  delay: 1.5,
+  x: 200,
+});
+gsap.from('.navigator', { opacity: 0, duration: 1, delay: 2, y: -50 });
